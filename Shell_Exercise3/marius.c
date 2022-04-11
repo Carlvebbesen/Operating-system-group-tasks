0b6112ddccdca02131450fe42c2d24f121326105
mariunsj <mariunsj@stud.ntnu.no>
2022-04-11 13:47:07 +0200

    Jeg er en legende

--- Shell_Exercise3/marius.c
+++ Shell_Exercise3/marius.c
@@ -3,49 +3,44 @@
 #include <limits.h>
 #include <string.h>
 
-/*
-
-char *splitPath(char *path)
+char *getWorkingDir()
 {
-    char *param;
-    const char *arguments[10];
-    int i = 0;
-    char delemit[] = " \t";
-    param = strtok(path, delemit);
-    if (param != NULL)
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
     {
-        arguments[i] = param;
-        i++;
+        return cwd;
     }
-    while (param != NULL)
+    else
     {
-        param = strtok(NULL, delemit);
-        if (param != NULL)
-        {
-            arguments[i] = param;
-            i++;
-        }
+        perror("getcwd() error");
+        return "error";
     }
-    return arguments;
 }
-*/
+
+int changeDir(const char *path) {
+    int dirVal = chdir(path);
+    if (dirVal == -1) {
+        printf("cd: no such file or directory: %s\n", path);
+    }
+}
 
 int main()
 {
-    char *enteredText;
+    char command[25];
     char cwd[PATH_MAX];
-    char command[100];
-    char *arguments[10];
     while (1)
     {
         if (getcwd(cwd, sizeof(cwd)) != NULL)
         {
-            bzero(enteredText, 25);
+            bzero(command, 25);
             printf("%s: ", cwd);
-            scanf("%25s", enteredText);
-            printf("Your entered text was: %s\n", enteredText);
-            //strcpy(command, *splitPath(enteredText));
-            //printf("Your command was: %s \n", command);
+            scanf("%25s", command);
+            //printf("Your command was: %s\n", command);
+            if (strcmp(command, "cd")) {
+                changeDir(command);
+            }
+
+            fflush(stdout);
         }
         else
         {
