8c0839cdb08a8f7987d890ef8b9321b8872f464a
Henrik Brun Fevang <henribfe@stud.ntnu.no>
2022-04-11 14:16:01 +0200

    Using fork on new command execs

---
+++ Shell_Exercise3/henrik.c
@@ -0,0 +1,78 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+char *getWorkingDir()
+{
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+    {
+        return cwd;
+    }
+    else
+    {
+        perror("getcwd() error");
+        return "error";
+    }
+}
+
+int executeCommand(char *inputBuffer)
+{
+    char *command;
+    char *newArg;
+    char **args;
+    strcpy(strtok(inputBuffer, " "), command);
+    int i = 0;
+    newArg = strtok(NULL, " ");
+    while (newArg != NULL)
+    {
+        args[i] = newArg;
+        i++;
+        newArg = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+    execv(command, args);
+    exit(0);
+}
+
+int main()
+{
+    char inputBuffer[25];
+    char cwd[PATH_MAX];
+    int status;
+    while (1)
+    {
+        if (getcwd(cwd, sizeof(cwd)) != NULL)
+        {
+            bzero(inputBuffer, 25);
+            printf("%s: ", cwd);
+            fgets(inputBuffer, 25, stdin);
+            if (feof(stdin))
+            {
+                exit(0);
+            }
+            inputBuffer[strcspn(inputBuffer, "\n")] = 0;
+            if (fork() == 0)
+            {
+                executeCommand(inputBuffer);
+            }
+            else
+            {
+                waitpid(-1, &status, 0);
+                printf("Exit status [%s] = %d \n", inputBuffer, status);
+            }
+            fflush(stdout);
+        }
+        else
+        {
+            perror("getcwd() error");
+            return 0;
+        }
+    }
+
+    return 0;
+}
